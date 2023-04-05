c5bc619971338aa32569aaedfae215ef6ec4c6c5
DarkLeZx <depniel08@mhs.mdp.ac.id>
2023-04-05 09:22:29 +0700

    05/04/2023

--- app/src/main/java/com/example/kuliner_kita/Model/ModelResponse.java
+++ app/src/main/java/com/example/kuliner_kita/Model/ModelResponse.java
@@ -1,5 +1,20 @@
 package com.example.kuliner_kita.Model;
 
+import java.util.List;
+
 public class ModelResponse {
-    
+    private  String kode, pesan;
+    private List<ModelKulinar> data;
+
+    public String getKode() {
+        return kode;
+    }
+
+    public String getPesan() {
+        return pesan;
+    }
+
+    public List<ModelKulinar> getData() {
+        return data;
+    }
 }
