4e23e283c928b718e3e9cc7be7aaa6a751702718
DarkLeZx <depniel08@mhs.mdp.ac.id>
2023-04-17 09:23:04 +0700

    Gradle, API

---
+++ app/src/main/java/com/example/kuliner_kita/API/RetroServer.java
@@ -0,0 +1,21 @@
+package com.example.kuliner_kita.API;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetroServer {
+    private  static  final String alamatServer = "https://kulinerdns.000webhostapp.com/";
+    private  static Retrofit retro;
+
+    public static  Retrofit konekRetrofit()
+    {
+        if(retro==null)
+        {
+            retro = new Retrofit.Builder()
+                    .baseUrl(alamatServer)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retro;
+    }
+}
